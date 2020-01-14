f5633fd31ae8889300515241f2ee8b43463ec21a
github username <github email address>
2020-01-14 10:59:11 -0600

    needs post

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        "Accept": "application/json"
-      }
-    })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    // let data = {}
+    
+    // let toConfig = {
+    //   method: "POST",
+    //   headers: {
+    //     "Accept": "application/json"
+    //   },
+    //   body: JSON.stringify(data)
+    // }
+
+    return fetch('https://icanhazdadjoke.com/', {headers: {
+          "Accept": "application/json"
+        }})
+    .then(resp => resp.json())
+    .then(json => joke = json.joke)
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
+    event.preventDefault()
+    // if(username === "") return;
+    const username = document.getElementById('name-input').value
+    newJokeLi.innerHTML = `<span class="username">${username} says:</span> ${joke}`
     jokeList.appendChild(newJokeLi)
+
+    fetchJoke()
+    
   })
 })
